ac8dc14cc7622f4d49db1ed7c34d628deb373dfc
Rayan <92063117+iiRyan@users.noreply.github.com>
2023-10-12 10:54:00 +0300

    DAY 4
    Error Handling

---
+++ ErrorHandlingExample/src/main/java/com/errorhandling/servlets/ErrorHandling.java
@@ -0,0 +1,42 @@
+package com.errorhandling.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.errorhandlong.exception.MyException;
+
+/**
+ * Servlet implementation class ErrorHandling
+ */
+public class ErrorHandling extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ErrorHandling() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		throw new MyException();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ErrorHandlingExample/src/main/java/com/errorhandlong/exception/MyException.java
@@ -0,0 +1,8 @@
+package com.errorhandlong.exception;
+
+import java.io.IOException;
+
+public class MyException extends IOException{
+	
+	
+}
